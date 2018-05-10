cf517a1b827467166b7e375773051e4905741087
nrocy777 <nrocy@protonmail.com>
2018-05-10 21:09:54 +0100

    use rapidjson for request payload

--- dicom.cpp
+++ dicom.cpp
@@ -10,28 +10,29 @@
  *
  */
 
+#include <iostream>
 #include <string>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
-
+#include <rapidjson/document.h>
 
 namespace dicom {
 
 	struct dicomhandler {
-		std::string	method;
-		bool (*actor)(boost::property_tree::ptree &request, boost::property_tree::ptree &reply);
+		std::string method;
+		bool (*actor)(const rapidjson::Document &request, boost::property_tree::ptree &reply);
 	};
 
-	static bool ping(boost::property_tree::ptree &request, boost::property_tree::ptree &reply) {
+	static bool ping(const rapidjson::Document &request, boost::property_tree::ptree &reply) {
 		reply.put("ping", "pong");
 		return true;
 	}
 
-	static bool echo(boost::property_tree::ptree &request, boost::property_tree::ptree &reply) {
-		reply.put("echo", request.get("params","Missing params!"));
+	static bool echo(const rapidjson::Document &request, boost::property_tree::ptree &reply) {
+		// TODO: param checks
+		reply.put("echo", request["params"].GetString());
 		return true;
 	}
-
 
 	static const struct dicomhandler dicom_handlers[] = {
 		{ "ping", ping },
@@ -39,20 +40,30 @@ namespace dicom {
 	};
 
 	std::string exec(boost::property_tree::ptree reqpt) {
-
 		boost::property_tree::ptree reppt;		   
 		reppt.put("dicom", "1.0");
-		reppt.put("type", "reply");
 
-		std::string method = reqpt.get<std::string>("method","");
+		// TODO: param checks
+		std::string payload = reqpt.get("payload", "");
+		std::string signature = reqpt.get("signature", "");
+		std::string pubkey = reqpt.get("pubkey", "");
+
+		rapidjson::Document d;
+		d.Parse(payload.c_str());
+
+		std::cout << payload << std::endl;
+
+		std::string method = d["method"].GetString();
 
 		unsigned int i = 0;
 		bool methodfound = false;
 		for (; i < (sizeof(dicom_handlers)/sizeof((dicom_handlers)[0])); i++) {
 			if (!method.compare(dicom_handlers[i].method)) {
 				methodfound = true;
-				bool rc = dicom_handlers[i].actor(reqpt, reppt);
-				if (!rc)	reppt.put("error", "DICOM call failed.");
+				bool rc = dicom_handlers[i].actor(d, reppt);
+				if (!rc) {
+					reppt.put("error", "DICOM call failed.");
+				}
 			}
 		}
 

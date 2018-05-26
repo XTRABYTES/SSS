53ec80287dd148c391c2aa062b17a771c272f773
Fluorine <joseph.anglada92@gmail.com>
2018-05-26 15:08:28 -0400

    Method to check JSON data from the Connect() method

--- client/csharp/DICOMClient.Tests/ClientConnectionTests.cs
+++ client/csharp/DICOMClient.Tests/ClientConnectionTests.cs
@@ -1,4 +1,5 @@
 using FluentAssertions;
+using Newtonsoft.Json;
 using NUnit.Framework;
 using System.Threading.Tasks;
 
@@ -11,13 +12,31 @@ namespace DICOMClient.Tests
         const int PORT = 8443;
 
         [Test]
-        public async Task ConnectTest()
+        public async Task ConnectReturnsDataTest()
         {
             var client = new DICOM.Client(HOST, PORT);
 
             var result = await client.Connect();
 
-            result.Should().NotBeNull();
+            result.Should().NotBeNullOrWhiteSpace();
+        }
+
+        [Test]
+        public async Task ConnectReturnsValidDataTest()
+        {
+            var client = new DICOM.Client(HOST, PORT);
+
+            var result = await client.Connect();
+
+            dynamic data = JsonConvert.DeserializeObject(result);
+
+            string method = data.method;
+            string sessionId = data.session_id;
+            string publicKey = data.pubkey;
+
+            method.Should().Be("connect");
+            sessionId.Should().NotBeNullOrWhiteSpace();
+            publicKey.Should().Contain("BEGIN PUBLIC KEY").And.Contain("END PUBLIC KEY");
         }
     }
 }
